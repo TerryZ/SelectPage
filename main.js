04772a71b678a9dfa0132dda12e78bec59a6f34f
TerryZ <terry5@foxmail.com>
2022-08-12 00:41:25 +0800

    feat: 增加 bootstrap 作为开发模式下的界面基础样式

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      Vite
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      javascript
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button">do some thing</button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+
 `
